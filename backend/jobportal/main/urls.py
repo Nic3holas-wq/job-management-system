b37c61e97c4d08dcafebf36975ba148183d0ca42
Nic3holas-wq <nicholasmurimi254@gmail.com>
2025-08-01 09:06:01 +0300

    fix profile not visible on UI, Issue was wrong placement of urlpatterns+

--- backend/jobportal/main/urls.py
+++ backend/jobportal/main/urls.py
@@ -1,7 +1,7 @@
-# main/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import JobViewSet, register_user, get_user_profile
+from .views import JobViewSet, register_user, get_user_profile, upload_profile_picture
+
 
 router = DefaultRouter()
 router.register('jobs', JobViewSet, basename='job')
@@ -10,4 +10,9 @@ urlpatterns = [
     path('', include(router.urls)),
     path('register/', register_user, name='register_user'),
     path('user/', get_user_profile, name='user-profile'),
+    path('user/upload-picture/', upload_profile_picture, name='upload-picture'),
 ]
+
+# ✅ Add media route only if in development mode
+
+
